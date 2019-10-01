65821e3e134be84cbb94cf645004d6de918656b3
Thijs Brobbel <thijsbrobbel@gmail.com>
2019-10-01 15:27:59 +0200

    Get the testShowcase.sh script working on Windows.

--- testShowcase.sh
+++ testShowcase.sh
@@ -9,7 +9,7 @@ reqPath="/oauth2/token"
 body="grant_type=client_credentials&scope=greetings%3Aview"
 digest=$(echo -n ${body} | openssl dgst -binary -sha256 | openssl base64)
 
-reqId=`uuidgen`
+reqId=`powershell -Command "[guid]::NewGuid().ToString()"`
 
 signingString1="(request-target): ${httpMethod} ${reqPath}"
 signingString2="date: ${reqDate}"
@@ -37,7 +37,7 @@ reqPath="/greetings/single"
 body=""
 digest=$(echo -n ${body} | openssl dgst -binary -sha256 | openssl base64)
 
-reqId=`uuidgen`
+reqId=`powershell -Command "[guid]::NewGuid().ToString()"`
 
 signingString1="(request-target): ${httpMethod} ${reqPath}"
 signingString2="date: ${reqDate}"
@@ -48,7 +48,7 @@ signingString="${signingString1}"$'\n'"${signingString2}"$'\n'"${signingString3}
 
 signature=`printf "${signingString}" | openssl dgst -sha256 -sign ./example_client_signing.key -passin "pass:changeit" | openssl base64 -A`
 
-accessToken=$(echo ${response} | jq --raw-output '.access_token')
+accessToken=$(echo ${response} | json access_token)
 
 response2=`curl -X GET --silent --cert example_client_tls.cer --key example_client_tls.key \
 -H "Date: ${reqDate}" \
@@ -60,8 +60,8 @@ response2=`curl -X GET --silent --cert example_client_tls.cer --key example_clie
 -H "Accept: application/json" \
 https://api.ing.com/greetings/single`  2> /dev/null
 
-message=$(echo ${response2} | jq --raw-output '.message')
-id=$(echo ${response2} | jq --raw-output '.id')
-messageTimestamp=$(echo ${response2} | jq --raw-output '.messageTimestamp')
+message=$(echo ${response2} | json message)
+id=$(echo ${response2} | json id)
+messageTimestamp=$(echo ${response2} | json messageTimestamp)
 
 echo $message " at " $messageTimestamp " with id " $id
